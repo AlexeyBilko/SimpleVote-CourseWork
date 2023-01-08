3063f1ba823696a8e2017b3510f8c8925e6c178c
Alexey Bilko <bilkoalexey67@gmail.com>
2023-01-08 19:56:31 +0200

    chart js displays answers for questions with known answers

---
+++ SimpleVote.UI/wwwroot/js/fromHtmlEntities.js
@@ -0,0 +1,11 @@
+﻿String.fromHtmlEntities = function (string) {
+    return (string + "").replace(/&#\d+;/gm, function (s) {
+        return String.fromCharCode(s.match(/\d+/gm)[0]);
+    })
+};
+
+function decodeHTMLEntities(text) {
+    var textArea = document.createElement('textarea');
+    textArea.innerHTML = text;
+    return textArea.value;
+}
